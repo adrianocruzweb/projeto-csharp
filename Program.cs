983b614c22ed86c27d6e482eae7b79748b7b7f59
adrianocruzweb <adrianocruzweb@hotmail.com>
2023-11-14 13:10:49 -0400

    tratamento na variavel altura double

--- Program.cs
+++ Program.cs
@@ -17,6 +17,6 @@ bool condicao = true;
 
 Console.WriteLine(apresentacao);
 Console.WriteLine("Valor da variavel quantidade: "+quantidade);
-Console.WriteLine("Valor da variavel altura: "+altura);
+Console.WriteLine("Valor da variavel altura: "+altura.ToString("0.00"));
 Console.WriteLine("Valor da variavel preco: "+preco);
 Console.WriteLine("Valor da variavel condicao: "+condicao);
