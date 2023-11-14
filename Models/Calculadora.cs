a248b69a4561b20dca2fbc6f09399bd74ca07027
adrianocruzweb <adrianocruzweb@hotmail.com>
2023-11-14 19:12:08 -0400

    opradores aritmeticos basicos

---
+++ Models/Calculadora.cs
@@ -0,0 +1,25 @@
+namespace projeto_csharp.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+    }
+}
