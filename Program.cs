7e395a110b4405a6f7bcfd41ec7ee5fbc7d60f25
adrianocruzweb <adrianocruzweb@hotmail.com>
2023-11-14 13:08:58 -0400

    A explicação dos tipos de variaveis

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
-﻿using projeto_csharp.Models;
+﻿/*using projeto_csharp.Models;
 
 Pessoa pessoaUm = new Pessoa();
 
 pessoaUm.Nome = "Adriano";
 pessoaUm.Idade = 35;
 
-pessoaUm.Apresentar();
+pessoaUm.Apresentar();*/
+
+using Microsoft.Win32.SafeHandles;
+
+string apresentacao = "Olá, Seja bem vindo";
+int quantidade = 2;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variavel quantidade: "+quantidade);
+Console.WriteLine("Valor da variavel altura: "+altura);
+Console.WriteLine("Valor da variavel preco: "+preco);
+Console.WriteLine("Valor da variavel condicao: "+condicao);
