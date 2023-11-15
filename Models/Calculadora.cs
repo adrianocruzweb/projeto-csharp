ce483093ff797dac4b469fcfa2f56481ff977900
adrianocruzweb <adrianocruzweb@hotmail.com>
2023-11-15 07:42:31 -0400

    ajuste com texto de odem execução

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -64,9 +64,9 @@ namespace projeto_csharp.Models
 
         public void tabuadaCincoWhile(){
             int numero = 5;
-            int contador = 0;
+            int contador = 1;
             while (contador <= 10){
-                Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+                Console.WriteLine($"{contador}ª Execução - {numero} x {contador} = {numero * contador}");
                 contador++;
             }
         }
