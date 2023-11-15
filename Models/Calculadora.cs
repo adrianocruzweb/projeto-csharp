02c5d9153076f58a13e4f5f8e2016332665c9dc6
adrianocruzweb <adrianocruzweb@hotmail.com>
2023-11-15 07:44:44 -0400

    usando o break

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -68,6 +68,11 @@ namespace projeto_csharp.Models
             while (contador <= 10){
                 Console.WriteLine($"{contador}ª Execução - {numero} x {contador} = {numero * contador}");
                 contador++;
+
+                if(contador == 5)
+                {
+                    break;
+                }
             }
         }
     }
