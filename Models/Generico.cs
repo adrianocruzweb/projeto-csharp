b25df8fbd002dcfa8371d9abf59247bef4060d1a
adrianocruzweb <adrianocruzweb@hotmail.com>
2023-11-19 17:20:51 -0400

    uilizando opções de formatação

--- Models/Generico.cs
+++ Models/Generico.cs
@@ -15,8 +15,27 @@ namespace projeto_csharp.Models
             decimal valor = 1582.40M;
 
 
+            /*
+                se eu coloco somente esse trecho "C" com ou sem numero ele reperesenta somente a formatação
+                de valor monetário(corrency) da configuração acima, com o número logo a frente ele
+                reperesenta as casas decimais, co "CurrentInfo...." ele represeta uma personalização pontual
+                do tipo de moeda 'Dolar'
+            */
+            Console.WriteLine(valor.ToString("C8", CultureInfo.CreateSpecificCulture("en-US")));
 
-            Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine(valor.ToString("C8"));
+
+            Console.WriteLine(valor.ToString("N3"));
+
+            Console.WriteLine(valor.ToString("N"));
+
+            valor = .15M;
+
+            Console.WriteLine(valor.ToString("P"));
+
+            int numero = 121212;
+
+            Console.WriteLine(numero.ToString("##-##-##"));
         }
     }
 }
